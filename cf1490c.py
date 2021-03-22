8a6252eb3a72a9645d8b08bbc163fe037cc88734
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-03-22 11:36:50 +0600

    optimised soln

--- cf1490c.py
+++ cf1490c.py
@@ -17,24 +17,56 @@ def pl(a):
 		cout(str(val) + ' ')
 	cout('\n')
 		
+def binSearch(l, val, start, end):
+	if end < start:
+		return False
 
+	mid = (start + end) // 2
+
+	if l[mid] == val:
+		return True
+	elif l[mid] < val:
+		return binSearch(l, val, mid+1, end)
+	elif l[mid] > val:
+		return binSearch(l, val, start, mid-1)
 
 #main
 
  
 t, = mp()
+a = []
+
+a = {i**3 for i in range(1, 10000)}
+
 for _ in range(t):
 	x, = mp()
 
-	p = int(x**(1/3))
-	k = 0
-
-	for i in range(1, p+1):
-		y = x - i*i*i
+	#p = int(x**(1/3))
+	#k = 0
+	'''
+	for val in a:
+		if val > x:
+			cout('No\n')
+			break
+		if binSearch(a, x-val, 0, 9999):
+			cout('Yes\n')
+			break
+	'''
+	for val in a:
+		if x-val in a:
+			cout('Yes\n')
+			break
+	#for i in range(1, p+1):
+	#	y = x - i*i*i
+		'''
 		z = int(y**(1/3))
 		if z > 0 and (z*z*z == y or (z+1)*(z+1)*(z+1) == y):
 			cout('Yes\n')
 			break
+		'''
+	#	if binSearch(a, y, 0,9999):
+	#		cout('Yes\n')
+	#		break
 	else:
 		cout('No\n')
 	'''
