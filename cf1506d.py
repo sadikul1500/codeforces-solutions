2df0747b931d7b6878fd497ad45efdd8638c21d2
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2021-03-29 15:09:23 +0600

    accepted

---
+++ cf1506d.py
@@ -0,0 +1,67 @@
+#input template
+from sys import stdin, stdout
+from collections import Counter, OrderedDict, defaultdict
+import math
+import re
+cin = stdin.readline
+cout = stdout.write
+mp = lambda: list(map(int, cin().split()))
+ 
+def chars(): #function for taking string input as character array since string in python is immutable
+    s = cin()
+    return(list(s[:len(s) - 1]))
+ 
+#print list	
+def pl(a):	
+	for val in a:
+		cout(str(val) + ' ')
+	cout('\n')
+		
+def binSearch(l, val, start, end):
+	if end < start:
+		return False
+
+	mid = (start + end) // 2
+
+	if l[mid] == val:
+		return True
+	elif l[mid] < val:
+		return binSearch(l, val, mid+1, end)
+	elif l[mid] > val:
+		return binSearch(l, val, start, mid-1)
+
+def myLog(x, y):
+	z = int(math.log(x, y))
+
+	if y**(z+1) > x:
+		return z
+	elif y**(z+1) == x:
+		return z+1
+
+#main
+
+ 
+t, = mp()
+for _ in range(t):
+	n, = mp()
+	a = mp()
+
+	d = {}
+	maxi = 0
+
+	for item in a:
+		if item in d:
+			d[item] += 1
+
+		else:
+			d[item] = 1
+		maxi = max(maxi, d[item])
+
+	if maxi <= n // 2:
+		if n % 2:
+			cout('1\n')
+		else:
+			cout('0\n')
+	else:
+		cout(str(2*maxi-n) + '\n')
+
